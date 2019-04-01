6e121029972e3eca52067f9fef76cc71fb25af13
laumlin <zhengkanglin@itiger.com>
2019-04-01 15:55:48 +0800

    post formdata的两种方法axios和fetch

--- controllers/verify.js
+++ controllers/verify.js
@@ -1,3 +1,4 @@
+const fetch = require('node-fetch');
 const axios = require('axios');
 const config = require('../config');
 const fs = require('fs');
@@ -13,19 +14,29 @@ class Verify {
         'idcard_number': idcard_number, 'face_image_type': 'raw_image', 'image':  image};
         
         let data = createFormData(formData)
+        
+        // 方法一使用fetch
+        // await fetch('http://localhost:8000/verify', { method: 'POST', body: data })
+        //     .then(res => {
+        //         return res.json();
+        //     })
+        //     .then(json => {
+        //         ctx.response.body = json
+        //         if (json && json.error_message) {
+        //             ctx.status = 400
+        //             ctx.response.body = json.error_message
+        //         }
+        //         ctx.response.body = json
+        //     })
 
-        // let data = new FormData();
-        // data.append('api_key', config.api_key)
-        // data.append('api_secret', config.api_secret)
- 
-        await axios.post(config.baseURL, data, { headers:{'Content-Type': 'multipart/form-data'}})
+        // 方法二使用axios
+        await axios.post(config.baseURL, data, { headers: data.getHeaders() })
             .then(res => {
-                console.log('ok')
-                ctx.response.body = res.data;
+                ctx.response.body = res.data
             })
             .catch(err => {
-                // console.log(err)
-                ctx.response.body = err.response.data.error_message
+                ctx.status = 400
+                ctx.response.body = err.response.data
             })
 
     }
